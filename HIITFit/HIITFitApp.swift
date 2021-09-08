ffaede8da63cc1f1e6526720d4468c689725a0c2
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-09-08 14:50:14 +0800

    add button style to all buttons

--- HIITFit/HIITFitApp.swift
+++ HIITFit/HIITFitApp.swift
@@ -69,6 +69,7 @@ struct HIITFitApp: App {
                           support@xyz.com
                         """))
                 }
+                .buttonStyle(RaisedButtonStyle())
         }
     }
 }
